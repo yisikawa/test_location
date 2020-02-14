0f7b26004191104dfb9ba270ec89ffbb8004cfea
Yoshiaki Ishikawa <yisikawa@ma.medias.ne.jp>
2020-02-14 09:32:46 +0900

    change MAP API to leaflet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 import 'package:intl/intl.dart';
 import 'dart:io';
 import 'dart:async';
@@ -49,7 +50,6 @@ class _MapPageState extends State<MapPage> {
   void initState() {
     super.initState();
     _getAuth();
-//    _selectedChoice = _data[0];
   }
 
   Future<void> _getAuth() async {
@@ -63,9 +63,6 @@ class _MapPageState extends State<MapPage> {
     jsonList.forEach((value) {
       Map _temp = value;
       _data.add(_temp['user_id']);
-    });
-    _data.forEach((val) {
-      print('_data = ' + val);
     });
   }
 
@@ -103,12 +100,6 @@ class _MapPageState extends State<MapPage> {
     }
   }
 
-  Completer<GoogleMapController> _controller = Completer();
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -141,8 +132,8 @@ class _MapPageState extends State<MapPage> {
         IconButton(
             icon: Icon(Icons.calendar_today),
             onPressed: () => _selectDate(context)),
-        IconButton(icon: Icon(Icons.directions_walk), onPressed: null),
         IconButton(icon: Icon(Icons.watch_later), onPressed: null),
+        IconButton(icon: Icon(Icons.directions_walk), onPressed: null),
         IconButton(
           icon: Icon(Icons.location_on),
           onPressed: null,
@@ -152,13 +143,34 @@ class _MapPageState extends State<MapPage> {
   }
 
   Widget _mapView(BuildContext context) {
-    return new GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        // 最初のカメラ位置
-        target: LatLng(35.000081, 137.004055),
+    return new FlutterMap(
+      options: new MapOptions(
+        center: new LatLng(35.000081, 137.004055),
         zoom: 17.0,
       ),
+      layers: [
+        new TileLayerOptions(
+          urlTemplate: "https://api.tiles.mapbox.com/v4/"
+              "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+          additionalOptions: {
+            'accessToken':
+                'pk.eyJ1IjoieWlzaWthd2EiLCJhIjoiY2s2YndmdXFuMGZ1bDNsb3ZnMXBsbnI3eSJ9.gftC8NPsB9xNWIVEdWnTvw',
+            'id': 'mapbox.streets',
+          },
+        ),
+        new MarkerLayerOptions(
+          markers: [
+            new Marker(
+              width: 20.0,
+              height: 20.0,
+              point: new LatLng(35.000081, 137.004055),
+              builder: (ctx) => new Container(
+                child: new FlutterLogo(),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
